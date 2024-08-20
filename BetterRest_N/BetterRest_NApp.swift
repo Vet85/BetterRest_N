d868ac48bb20e07e6c9ebb1857548cf955971470
Vitaliy Novichenko <Novichenkovitaliy@gmail.com>
2024-08-20 14:31:01 +0300

    Initial Commit

---
+++ BetterRest_N/BetterRest_NApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_NApp.swift
+//  BetterRest_N
+//
+//  Created by Vitaliy Novichenko on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_NApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
